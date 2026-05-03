24bbec9a2f8487c0b5c7cb1eb32a08d5404a1f0d
Ula Chao <gary0626@gmail.com>
2026-05-04 02:38:08 +0800

    fix: set next.config basePath

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/zookeeper",
+  assetPrefix: '/zookeeper',
 };
 
 export default nextConfig;
